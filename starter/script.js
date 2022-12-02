35311fd79104b05505d5ef216aef62bf76fb38ec
cedagbo <cericdagbo@outlook.com>
2022-12-02 20:26:23 +0000

    add pseudocoding

---
+++ starter/script.js
@@ -0,0 +1,18 @@
+// Pseucoding to implement the code quiz
+// Create a button that would enable the user to start the game
+// Prompt the user for his input
+// Link the start button to another html page where we provide different guess options to the user
+// Set a time limit that prompts the user for his input
+// When the user clicks on the start button, the time should reset
+// When a user presses a letter key, the user's guess should be captured as a key event.
+// Create a key event that captures the user word guess -> i.e. when the user presses a key
+// Initialise the wins counts
+// The user wins after guessing all the letters
+// A message should appear and the timer should stop
+// The corresponding blank "_" should be replaced by the letter
+// Initialise the losses counts
+// The user looses when the timer runs out before he guesses all the letters
+// A message should appear and the timer should stop
+// Display the wins and losses status on the screen to enable the user to track his score
+// Ensure the score remains the same when the user refreshes the browser page
+// Bonus! Add a reset button to sets the wins and losses counts back to zero
